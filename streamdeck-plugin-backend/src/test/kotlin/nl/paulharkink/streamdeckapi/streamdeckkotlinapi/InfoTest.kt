ca1d92a0e2ee5de0db1ed45a6e919f91ecc44e28
Paul Harkink <paul.harkink@fdmediagroep.nl>
2023-09-11 16:14:13 +0200

    - Refactor from StringSpec to FunSpec

--- streamdeck-plugin-backend/src/test/kotlin/nl/paulharkink/streamdeckapi/streamdeckkotlinapi/InfoTest.kt
+++ streamdeck-plugin-backend/src/test/kotlin/nl/paulharkink/streamdeckapi/streamdeckkotlinapi/InfoTest.kt
@@ -3,16 +3,17 @@ package nl.paulharkink.streamdeckapi.streamdeckkotlinapi
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.module.paramnames.ParameterNamesModule
 import io.github.projectmapk.jackson.module.kogera.KotlinModule
-import io.kotest.core.spec.style.StringSpec
+import io.kotest.core.spec.style.FunSpec
 import io.kotest.matchers.shouldBe
 
 
-class InfoTest : StringSpec({
-    val objectMapper: ObjectMapper = ObjectMapper()
+class InfoTest : FunSpec() {
+
+    private val objectMapper: ObjectMapper = ObjectMapper()
         .registerModule(KotlinModule.Builder().build())
         .registerModule(ParameterNamesModule())
 
-    val applicationJson = """
+    private val applicationJson = """
         {
     "font": ".AppleSystemUIFont",
     "language": "en",
@@ -21,7 +22,7 @@ class InfoTest : StringSpec({
     "version": "6.3.0.18948"
   }
     """.trimIndent()
-    val application = Application(
+    private val application = Application(
         font = ".AppleSystemUIFont",
         language = "en",
         platform = Platform.MAC,
@@ -29,7 +30,7 @@ class InfoTest : StringSpec({
         version = "6.3.0.18948"
     )
 
-    val colorsJson = """
+    private val colorsJson = """
         {
     "buttonPressedBackgroundColor": "#303030FF",
     "buttonPressedBorderColor": "#646464FF",
@@ -40,7 +41,7 @@ class InfoTest : StringSpec({
   }
     """.trimIndent()
 
-    val colors = Colors(
+    private val colors = Colors(
         buttonPressedBackgroundColor = ColorCode("#303030FF"),
         buttonPressedBorderColor = ColorCode("#646464FF"),
         buttonPressedTextColor = ColorCode("#969696FF"),
@@ -49,7 +50,7 @@ class InfoTest : StringSpec({
         mouseDownColor = ColorCode("#2EA8FFFF")
     )
 
-    val deviceOneJson = """
+    private val deviceOneJson = """
         {
       "id": "10458C7F2BAC0281A90818DBD247F874",
       "name": "Stream Deck",
@@ -61,26 +62,26 @@ class InfoTest : StringSpec({
     }
     """.trimIndent()
 
-    val deviceOne = Device(
+    private val deviceOne = Device(
         id = "10458C7F2BAC0281A90818DBD247F874",
         name = "Stream Deck",
         size = DeviceDimensions(5, 3),
         type = 0
     )
 
-    val pluginJson = """
+    private val pluginJson = """
         {
     "uuid": "nl.paulharkink.esd.kotlin",
     "version": "1.0.2"
   }
     """.trimIndent()
 
-    val plugin = Plugin(
+    private val plugin = Plugin(
         uuid = "nl.paulharkink.esd.kotlin",
         version = "1.0.2"
     )
 
-    val infoJson = """
+    private val infoJson = """
         {
   "application": $applicationJson,
   "colors": $colorsJson,
@@ -92,7 +93,7 @@ class InfoTest : StringSpec({
 }
     """.trimIndent()
 
-    val info = Info(
+    private val info = Info(
         application = application,
         colors = colors,
         devicePixelRatio = 1,
@@ -100,43 +101,45 @@ class InfoTest : StringSpec({
         plugin = plugin
     )
 
-    "should parse Plugin" {
-        val res = objectMapper.readValue(pluginJson, Plugin::class.java)
-        res shouldBe plugin
-    }
+    init {
+        test("should parse Plugin") {
+            val res = objectMapper.readValue(pluginJson, Plugin::class.java)
+            res shouldBe plugin
+        }
 
-    "should parse Application" {
-        val res = objectMapper.readValue(applicationJson, Application::class.java)
-        res shouldBe application
-    }
+        test("should parse Application") {
+            val res = objectMapper.readValue(applicationJson, Application::class.java)
+            res shouldBe application
+        }
 
-    "should parse Color" {
-        val res = objectMapper.readValue("\"#303030FF\"", ColorCode::class.java)
-        res shouldBe ColorCode("#303030FF")
-    }
+        test("should parse Color") {
+            val res = objectMapper.readValue("\"#303030FF\"", ColorCode::class.java)
+            res shouldBe ColorCode("#303030FF")
+        }
 
-    "should parse Device" {
-        val res = objectMapper.readValue(deviceOneJson, Device::class.java)
-        res shouldBe deviceOne
-    }
+        test("should parse Device") {
+            val res = objectMapper.readValue(deviceOneJson, Device::class.java)
+            res shouldBe deviceOne
+        }
 
-    "should parse Colors" {
-        val res = objectMapper.readValue(colorsJson, Colors::class.java)
-        res shouldBe colors
-    }
+        test("should parse Colors") {
+            val res = objectMapper.readValue(colorsJson, Colors::class.java)
+            res shouldBe colors
+        }
 
-    "should parse Info" {
-        val res = objectMapper.readValue(infoJson, Info::class.java)
-        res shouldBe info
-    }
+        test("should parse Info") {
+            val res = objectMapper.readValue(infoJson, Info::class.java)
+            res shouldBe info
+        }
 
-    "should parse Single Line Json" {
-        val res = objectMapper.readValue(
-            """
+        test("should parse Single Line Json") {
+            val res = objectMapper.readValue(
+                """
             {"application":{"font":".AppleSystemUIFont","language":"en","platform":"mac","platformVersion":"13.5.1","version":"6.3.0.18948"},"colors":{"buttonPressedBackgroundColor":"#303030FF","buttonPressedBorderColor":"#646464FF","buttonPressedTextColor":"#969696FF","disabledColor":"#007AFF7F","highlightColor":"#007AFFFF","mouseDownColor":"#2EA8FFFF"},"devicePixelRatio":1,"devices":[{"id":"10458C7F2BAC0281A90818DBD247F874","name":"Stream Deck","size":{"columns":5,"rows":3},"type":0}],"plugin":{"uuid":"nl.paulharkink.esd.kotlin","version":"1.0.2"}}
         """.trimIndent(), Info::class.java
-        )
-        res shouldBe info
-    }
+            )
+            res shouldBe info
+        }
 
-})
+    }
+}
